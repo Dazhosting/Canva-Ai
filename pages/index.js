2994246278e2b068dcb60dbc51d609387ca23b7b
Vercel Team <pribadiarep@gmail.com>
2025-08-07 16:27:10 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           </button>
           {menuOpen && (
             <div style={styles.dropdown}>
-              <button style={styles.dropdownButton} onClick={() => window.location.href = './docsapi.html'}>
+              <button style={styles.dropdownButton} onClick={() => window.location.href = './document.html'}>
                 📄 Docs API
               </button>
             </div>
